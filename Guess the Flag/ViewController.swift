233616b62ff79502ad0001fecbbd09ef160c3ee3
Zain Haq <zain_07@live.com>
2016-09-23 22:20:57 -0400

    Init Commit

---
+++ Guess the Flag/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Zain-Ul Haq on 2016-05-04.
+//  Copyright © 2016 Zain-Ul Haq. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]();
+    var score = 0;
+    var correctAnswer = 0;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"];
+        
+        button1.layer.borderWidth = 1;
+        button2.layer.borderWidth = 1;
+        button3.layer.borderWidth = 1;
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        askQuestion();
+        
+    }
+    
+    func askQuestion(action:UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        self.title = countries[correctAnswer].uppercaseString
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonTapped(sender: AnyObject) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+
+}
+
